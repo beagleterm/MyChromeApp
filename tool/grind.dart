bfc92e912a166c094940ee456469ed452be2e22e
limasdf <limasdf@ubuntu.(none)>
2014-03-09 10:46:24 -0700

    make ./grind deploy work

--- tool/grind.dart
+++ tool/grind.dart
@@ -13,11 +13,26 @@ void main([List<String> args]) {
   startGrinder(args);
 }
 
-void deploy(GrinderContext context) {
+void _compile([GrindContext context]) {
+  // Generate dart.js (doens't support eval), but actually use precompiled
+  // version(support eval) from chrome-dart bootstrap.
+  ProcessResult result =
+      Process.runSync('dart2js', ['nikes.dart', '--out=nikes.dart.js'],
+                      workingDirectory: BUILD_DIR.path);
+  if (result.stdout != null && !result.stdout.isEmpty) {
+    context.log(result.stdout);
+  }
+
+  // TODO(sunglim) : Remove deps, map and js output.
+}
+
+void deploy([GrinderContext context]) {
   if (context != null) {
     context.log("Copy App directory to BUILD directory");
   }
   BUILD_DIR.deleteSync(recursive: true);
   BUILD_DIR.createSync();
   copyDirectory(APP_DIR, BUILD_DIR);
+
+  _compile(context);
 }
